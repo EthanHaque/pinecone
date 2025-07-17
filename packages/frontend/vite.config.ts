a2cd4269c1de18d39a92a6ac407b50a9895eb4f8
Ethan Haque <ethan.k.haque@gmail.com>
2025-07-17 13:43:06 -0400

    feat: add shadcn support

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,19 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
+const filename: string = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(dirname, "./src"),
+    },
+  },
 });
