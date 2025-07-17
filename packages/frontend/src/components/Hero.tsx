b130e01b22cc1876df2270050316a1b753f51cba
Ethan Haque <ethan.k.haque@gmail.com>
2025-07-17 13:43:55 -0400

    feat: update landing page to use shadcn

---
+++ packages/frontend/src/components/Hero.tsx
@@ -0,0 +1,35 @@
+import { Badge } from "@/components/ui/Badge";
+import { Button } from "@/components/ui/Button";
+
+function Hero() {
+  return (
+    <div className="border-accent flex min-h-[calc(100vh-4rem)] w-full items-center justify-center overflow-hidden border-b">
+      <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center justify-between gap-x-10 gap-y-14 px-6 py-12 lg:flex-row lg:py-0">
+        <div className="max-w-xl">
+          <Badge className="rounded-full border-none py-1">Freshly Deployed</Badge>
+          <h1 className="xs:text-4xl mt-6 max-w-[20ch] text-3xl !leading-[1.2] font-bold tracking-tight sm:text-5xl lg:text-[2.75rem] xl:text-5xl">
+            Extract Insights From the Web
+          </h1>
+          <p className="xs:text-lg mt-6 max-w-[60ch]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+          </p>
+          <div className="mt-12 flex flex-col items-center gap-4 sm:flex-row">
+            <Button size="lg" className="w-full text-base sm:w-auto">
+              To Dashboard
+            </Button>
+            <Button variant="outline" size="lg" className="w-full text-base shadow-none sm:w-auto">
+              Sign Up
+            </Button>
+          </div>
+        </div>
+        <div className="hidden h-96 w-full max-w-xl rounded-lg bg-gray-200 lg:flex dark:bg-gray-800">
+          {/* Placeholder for an image or illustration */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
